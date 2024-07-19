930ca5c15e25960259fd5a441bc876bb626871c2
pommee <hugoh@hotmail.se>
2024-07-20 00:24:29 +0200

    feat: added settings page

--- application/widget/topbar.py
+++ application/widget/topbar.py
@@ -4,7 +4,7 @@ from textual.widgets import Label
 
 
 class TopBar(Container):
-    HELP_TEXT = "press [b highlight]?[/b highlight] for help "
+    HELP_TEXT = "[b yellow]?[/b yellow] Help  [b yellow]p[/b yellow] Settings "
 
     def __init__(self, app_version: str, statuses=""):
         super().__init__()
@@ -14,7 +14,7 @@ class TopBar(Container):
             id="topbar_title",
         )
         self.topbar_statuses = Label(statuses, id="topbar_statuses")
-        self.topbar_help = Label(self.HELP_TEXT, id="topbar_help")
+        self.topbar_help = Label(self.HELP_TEXT,  id="topbar_help")
 
     def compose(self) -> ComposeResult:
         yield self.topbar_title
